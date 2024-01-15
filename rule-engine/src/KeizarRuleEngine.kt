2df874050d33427867eae15dc8fd6db4fe63863d
jerryzhu <yuqian.zhu21@imperial.ac.uk>
2024-01-15 20:16:27 +0000

    modify BoardProperties to remove constants

--- rule-engine/src/KeizarRuleEngine.kt
+++ rule-engine/src/KeizarRuleEngine.kt
@@ -22,7 +22,7 @@ interface Board {
     val tiles: Flow<List<BoardPos>>
 }
 
-class BoardProperties(
+data class BoardProperties(
     val width: Int = 8,
     val height: Int = 8,
     val winningPos: BoardPos = BoardPos.fromString("d5"),
@@ -52,42 +52,7 @@ class BoardProperties(
         }
     }
 
-    companion object {        
-data class BoardProperties(
-    val width: Int,
-    val height: Int,
-    val winningPos: BoardPos,
-    val tileTypes: Map<BoardPos, TileType>
-) {
-    fun tileBackgroundColor(row: Int, column: Int): Boolean {
-        return if (row % 2 == 0) {
-            column % 2 != 0
-        } else {
-            column % 2 == 0
-        }
-    }
-
     companion object {
-        const val BOARD_SIZE = 8
-        const val WINNING_COUNT = 3
-
-        val KEIZAR_POS = BoardPos.fromString("d5")
-        val STARTING_PLAYER = Piece.Color.WHITE
-        val PIECES_STARTING_POS = listOf(
-            Piece.Color.WHITE to listOf(
-                "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
-                "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
-            ).map { BoardPos.fromString(it) },
-            Piece.Color.BLACK to listOf(
-                "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
-                "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
-            ).map { BoardPos.fromString(it) }
-        )
-        val FIXED_PLAIN_TILES_POS = listOf(
-            "a1", "b1", "g1", "h1", "c2", "d2", "e2", "f2",
-            "a8", "b8", "g8", "h8", "c7", "d7", "e7", "f7",
-        ).map { BoardPos.fromString(it) }
-
         fun random(random: Random = Random): BoardProperties {
             val width = 8
             val height = 8
