c18380428c6c72a23e552c407860ce8caf3d5807
Fengkai Liu <fl1121@ic.ac.uk>
2024-01-16 20:29:43 +0000

    Update `check standard tile arrangement` Test for Board Properties.

--- rule-engine/test/BoardPropertiesTest.kt
+++ rule-engine/test/BoardPropertiesTest.kt
@@ -35,15 +35,38 @@ class BoardPropertiesTest {
         assertEquals(ruleEngine.pieceAt(pos2), Player.WHITE)
         assertEquals(ruleEngine.pieceAt(pos1), null)
 
-
     }
 
-//    @Test
-//    fun `generate board 2`() {
-//        val prop = BoardProperties.getStandardProperties()
-//        assertEquals(8, prop.width)
-//        assertEquals(8, prop.height)
-////        assertEquals("d5", prop.winningPos.toString())
-////        assertEquals("{d5=KEIZAR, g7=KING, f4=QUEEN, a2=BISHOP, e6=KNIGHT, a7=ROOK}", prop.tileTypes.toString())
-//    }
+    @Test
+    fun `check standard tile arrangement`() {
+        val prop = BoardProperties.getStandardProperties(Random(1))
+        assertEquals("d5", prop.keizarTilePos.toString())
+        val arrangement = prop.tileArrangement
+        val checkList = MutableList(7){ MutableList(2){0} }
+        arrangement.forEach{
+            when(it.value){
+                TileType.KING -> if(it.key.color == TileColor.WHITE) checkList[0][0]++ else checkList[0][1] ++
+                TileType.QUEEN -> if(it.key.color == TileColor.WHITE) checkList[1][0]++ else checkList[1][1] ++
+                TileType.BISHOP -> if(it.key.color == TileColor.WHITE) checkList[2][0]++ else checkList[2][1] ++
+                TileType.KNIGHT -> if(it.key.color == TileColor.WHITE) checkList[3][0]++ else checkList[3][1] ++
+                TileType.ROOK -> if(it.key.color == TileColor.WHITE) checkList[4][0]++ else checkList[4][1] ++
+                TileType.KEIZAR -> if(it.key.color == TileColor.WHITE) checkList[5][0]++ else checkList[5][1] ++
+                TileType.PLAIN -> if(it.key.color == TileColor.WHITE) checkList[6][0]++ else checkList[6][1] ++
+            }
+        }
+        assertEquals(checkList[0][0], 1)
+        assertEquals(checkList[0][1], 1)
+        assertEquals(checkList[1][0], 1)
+        assertEquals(checkList[1][1], 1)
+        assertEquals(checkList[2][0], 2)
+        assertEquals(checkList[2][1], 2)
+        assertEquals(checkList[3][0], 2)
+        assertEquals(checkList[3][1], 2)
+        assertEquals(checkList[4][0], 2)
+        assertEquals(checkList[4][1], 2)
+        assertEquals(checkList[5][0], 1)
+        assertEquals(checkList[5][1], 0)
+        assertEquals(checkList[6][0], 23)
+        assertEquals(checkList[6][1], 24)
+    }
 }
