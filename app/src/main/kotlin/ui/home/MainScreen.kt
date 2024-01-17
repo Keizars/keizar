c5c954cfa0eee81ab8267d68ce9972a44479ca96
sz1321 <sz1321.ic.ac.uk>
2024-01-17 20:17:40 +0000

    changed the icon on home page to Keizar, and edited the button as required

--- app/src/main/kotlin/ui/home/MainScreen.kt
+++ app/src/main/kotlin/ui/home/MainScreen.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Chair
 import androidx.compose.material3.Button
@@ -16,12 +18,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import org.keizar.android.R
 import org.keizar.android.ui.game.GamePage
 
 @Composable
@@ -45,22 +49,40 @@ fun HomePage(navController: NavController) {
     ) {
         // Title or logo can be added here
         Box(modifier = Modifier) {
-            Icon(Icons.Default.Chair, "KEIZÁR", modifier = Modifier.size(48.dp))
+            Icon(
+                painter = painterResource(id = R.drawable.keizar_icon),
+                contentDescription = "Keizar Icon",
+                tint = Color.Unspecified
+            )
         }
 
         Spacer(modifier = Modifier.size(80.dp))
 
         // Single Player Button
-        Button(onClick = { navController.navigate("single player game") }, modifier = Modifier.width(150.dp)) {
-            Text("Single Player")
+        Button(onClick = { navController.navigate("single player game") }, modifier = Modifier.width(170.dp)) {
+            Text("Play vs Computer")
         }
 
 
         Spacer(modifier = Modifier.size(50.dp))
 
         // Multiplayer Button
-        Button(onClick = { /* TODO: Handle Multiplayer click */ }, modifier = Modifier.width(150.dp)) {
-            Text("Multiplayer")
+        Button(onClick = { /* TODO: Handle Multiplayer click */ }, modifier = Modifier.width(170.dp)) {
+            Text("2 players")
+        }
+
+        Spacer(modifier = Modifier.size(50.dp))
+
+        // Saved game Button
+        Button(onClick = { /* TODO: Handle Multiplayer click */ }, modifier = Modifier.width(170.dp)) {
+            Text("Saved games")
+        }
+
+        Spacer(modifier = Modifier.size(50.dp))
+
+        // Tutorial Button
+        Button(onClick = { /* TODO: Handle Multiplayer click */ }, modifier = Modifier.width(170.dp)) {
+            Text("Tutorial")
         }
     }
 }
