1a32b54fdd6d8556ed47e2b741e088040f760543
jerryzhu <yuqian.zhu21@imperial.ac.uk>
2024-01-18 19:05:30 +0000

    add tests for getAvailableTargets to GameSessionTest

--- rule-engine/test/GameSessionTest.kt
+++ rule-engine/test/GameSessionTest.kt
@@ -3,6 +3,7 @@ package org.keizar.game
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import kotlin.random.Random
 
@@ -16,5 +17,28 @@ class GameSessionTest {
         assertEquals(Player.BLACK, curPlayer.value)
         game.move(BoardPos("d7"), BoardPos("d6"))
         assertEquals(Player.WHITE, curPlayer.value)
+    }
+
+    @Test
+    fun `test getAvailableTargets`() = runTest {
+        val game = GameSession.create(Random(100))
+        val targets1 = game.getAvailableTargets(BoardPos("d2"))
+        val targets2 = game.getAvailableTargets(BoardPos("e2"))
+
+        targets1.collect { list ->
+            assertEquals(listOf("d3").map { BoardPos.fromString(it) }.toSet(), list.toSet())
+        }
+        targets2.collect { list ->
+            assertEquals(listOf("e3", "e4").map { BoardPos.fromString(it) }.toSet(), list.toSet())
+        }
+
+        assertTrue(game.move(BoardPos("d2"), BoardPos("d3")))
+        val targets3 = game.getAvailableTargets(BoardPos("d3"))
+        targets3.collect { list ->
+            assertEquals(
+                setOf("d2", "d4", "e3", "e4", "c3", "c4").map { BoardPos.fromString(it) }.toSet(),
+                list.toSet()
+            )
+        }
     }
 }
