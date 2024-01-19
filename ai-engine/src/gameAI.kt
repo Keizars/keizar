1bbe0aa67a85eb7729aafee2a9e050c25691d7b3
rl2121 <rl2121@ic.ac.uk>
2024-01-19 13:47:18 +0000

    Created GameAI Interface

--- ai-engine/src/gameAI.kt
+++ ai-engine/src/gameAI.kt
@@ -1,5 +1,15 @@
 package org.keizar.aiengine
 
+import org.keizar.game.BoardPos
+import org.keizar.game.BoardProperties
+import org.keizar.game.GameSession
+import org.keizar.game.Move
+import org.keizar.game.Player
+import org.keizar.game.boardPoses
 
 interface GameAI {
+    val game: GameSession
+    val myplayer: Player
+
+    fun FindBestMove():Move
 }
