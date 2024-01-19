1a2ee7c3d2a3c8bdff957ac880be2cb4e4a952df
rl2121 <rl2121@ic.ac.uk>
2024-01-19 16:41:26 +0000

    first impl of random game AI

--- ai-engine/src/gameAI.kt
+++ ai-engine/src/gameAI.kt
@@ -1,18 +1,21 @@
 package org.keizar.aiengine
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.toList
 import org.keizar.game.BoardPos
 import org.keizar.game.BoardProperties
 import org.keizar.game.GameSession
 import org.keizar.game.Move
 import org.keizar.game.Player
 import org.keizar.game.boardPoses
+import kotlin.random.Random
 
 interface GameAI {
     val game: GameSession
     val myplayer: Player
 
-    fun FindBestMove():Move
-    fun MakeMove():Move
+    suspend fun FindBestMove(): Pair<BoardPos,BoardPos>?
+    suspend fun MakeMove():Pair<BoardPos,BoardPos>?
 }
 
 class RandomGameAIImpl(
@@ -20,11 +23,25 @@ class RandomGameAIImpl(
     override val myplayer: Player
 ):GameAI {
 
-    override fun FindBestMove(): Move {
-        TODO("Not yet implemented")
+    override suspend fun FindBestMove():Pair<BoardPos,BoardPos>? {
+        val player = game.curPlayer.value
+        if (myplayer == player){
+            val myPieces = game.getAllPiecesPos(myplayer).toList()[0]
+
+            val randomItem = myPieces[Random.nextInt(myPieces.size)]
+
+            val validTargets = game.getAvailableTargets(randomItem).toList()[0]
+
+            val randomTarget = validTargets.random()
+
+            return Pair(randomItem, randomTarget)
+        }
+
+        else return null
+
     }
 
-    override fun MakeMove(): Move {
+    override suspend fun MakeMove(): Pair<BoardPos,BoardPos>? {
         val best_move = FindBestMove()
         return best_move
     }
