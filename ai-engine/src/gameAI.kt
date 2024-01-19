3b9aae09620564b5e44f087b2b8fab72db2a0db7
rl2121 <rl2121@ic.ac.uk>
2024-01-19 15:18:17 +0000

    Created RandomGameAIImpl class

--- ai-engine/src/gameAI.kt
+++ ai-engine/src/gameAI.kt
@@ -12,4 +12,22 @@ interface GameAI {
     val myplayer: Player
 
     fun FindBestMove():Move
+    fun MakeMove():Move
 }
+
+class RandomGameAIImpl(
+    override val game: GameSession,
+    override val myplayer: Player
+):GameAI {
+
+    override fun FindBestMove(): Move {
+        TODO("Not yet implemented")
+    }
+
+    override fun MakeMove(): Move {
+        val best_move = FindBestMove()
+        return best_move
+    }
+
+}
+
