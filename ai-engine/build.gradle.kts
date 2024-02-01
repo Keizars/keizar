ee734d50907e340f5d217a4b578da926c6cd0b3f
Fengkai Liu <fl1121@ic.ac.uk>
2024-02-01 18:24:26 +0000

    Update server

--- ai-engine/build.gradle.kts
+++ ai-engine/build.gradle.kts
@@ -7,9 +7,11 @@ dependencies {
     api(libs.kotlinx.serialization.json)
     implementation(project(":rule-engine"))
     implementation(libs.ktor.client.content.negotiation)
-    implementation(libs.ktor.server.cio)
+    implementation(libs.ktor.server.netty)
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
     testImplementation(libs.ktor.server.test.host)
+    runtimeOnly(libs.slf4j.simple)
+    implementation(libs.ktor.server.call.logging)
 }
--- ai-engine/src/protocol/StartServer.kt
+++ ai-engine/src/protocol/StartServer.kt
@@ -3,15 +3,31 @@ package org.keizar.aiengine.protocol
 import org.keizar.aiengine.protocol.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
-import io.ktor.server.cio.*
+import io.ktor.server.netty.Netty
+import io.ktor.server.plugins.callloging.CallLogging
+import io.ktor.server.request.uri
+import io.ktor.util.logging.KtorSimpleLogger
 
 fun main() {
-    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+    embeddedServer(
+        Netty,
+        port = 49152,
+        host = "0.0.0.0",
+        module = Application::module,
+        configure = {
+            this.tcpKeepAlive = true
+            this.connectionGroupSize = 40
+            this.workerGroupSize = 40
+            this.callGroupSize = 40
+        }
+    ).start(wait = true)
 }
+
 
 fun Application.module() {
     val context = setupServerContext()
+
+    install(CallLogging)
 
     configureSerialization()
     configureRouting(context)
