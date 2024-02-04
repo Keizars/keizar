847ed29e1d79d6234e159d875446ca330c7d3fec
Jerry Zhu <yz7821@ic.ac.uk>
2024-02-04 15:24:30 +0000

    change routing for training server to support modifying seed

--- server/src/training/plugins/Routing.kt
+++ server/src/training/plugins/Routing.kt
@@ -1,9 +1,11 @@
 package org.keizar.server.training.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.plugins.BadRequestException
 import io.ktor.server.request.receive
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.keizar.game.BoardProperties
 import org.keizar.server.training.RuleEngineAdaptor
 import org.keizar.server.training.ServerContext
 import org.keizar.game.Move
@@ -11,7 +13,10 @@ import org.keizar.game.Role
 
 fun Application.configureRouting(context: ServerContext) {
     routing {
-        get("/board") {
+        put("/board/{seed}") {
+            val seed = call.parameters["seed"]?.toIntOrNull()
+                ?: throw BadRequestException("Invalid seed")
+            context.boardProperties = BoardProperties.getStandardProperties(seed)
             call.respond(RuleEngineAdaptor.encodeBoard(context.boardProperties))
         }
         post("/moves/white") {
--- server/test/training/ServerTest.kt
+++ server/test/training/ServerTest.kt
@@ -7,7 +7,7 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.get
+import io.ktor.client.request.put
 import io.ktor.server.testing.testApplication
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -17,7 +17,7 @@ class ServerTest {
     @Test
     fun `test board`() = testApplication {
         application { module() }
-        val response = client.get("/board")
+        val response = client.put("/board/0")
         assertEquals(HttpStatusCode.OK, response.status)
     }
 
