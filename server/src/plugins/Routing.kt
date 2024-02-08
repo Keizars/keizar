f35d67ac451ac293b37f3c2d93e63d0e1f60f8ce
Him188 <Him188@mamoe.net>
2024-02-08 16:14:17 +0000

    Add get routing on /

--- server/src/plugins/Routing.kt
+++ server/src/plugins/Routing.kt
@@ -1,8 +1,17 @@
 package org.keizar.server.plugins
 
-import io.ktor.server.application.*
+import io.ktor.server.application.Application
+import io.ktor.server.application.call
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
 import org.keizar.server.routing.gameRoomRouting
 
 fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Server is running")
+        }
+    }
     gameRoomRouting()
 }
