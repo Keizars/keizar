4defea91bb7707453482b3fabf8a13f1ab8c7930
Him188 <Him188@mamoe.net>
2024-02-09 11:02:52 +0000

    Fix private rooms min height

--- app/src/main/kotlin/ui/game/mp/MultiplayerLobbyScene.kt
+++ app/src/main/kotlin/ui/game/mp/MultiplayerLobbyScene.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -180,7 +180,7 @@ private fun PlayWithFriendsSection(
             Modifier
                 .fillMaxWidth()
                 .align(Alignment.CenterHorizontally)
-                .height(92.dp),
+                .heightIn(min = 92.dp),
             contentAlignment = Alignment.CenterStart
         ) {
             val selfRoomId by vm.selfRoomId.collectAsState()
