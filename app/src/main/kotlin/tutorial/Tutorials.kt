8d796fae1244b9fe421715e98ec70321375bc289
Him188 <Him188@mamoe.net>
2024-02-22 22:58:48 +0000

    Refresher2 Make "You stopped opponent" and "capture" tooltip longer

--- app/src/main/kotlin/tutorial/Tutorials.kt
+++ app/src/main/kotlin/tutorial/Tutorials.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
-import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
@@ -190,7 +189,7 @@ object Tutorials {
                 showPossibleMovesThenMove { "e8" to "e6" }
                 removeTooltip()
                 tooltip {Text("You stopped opponent")}
-                delay(0.5.seconds)
+                delay(2.seconds)
                 removeTooltip()
             }
 
@@ -205,7 +204,7 @@ object Tutorials {
                 showPossibleMovesThenMove { "e6" to "f5" }
                 removeTooltip()
                 tooltip {Text("Capture")}
-                delay(0.5.seconds)
+                delay(2.seconds)
                 removeTooltip()
             }
 
