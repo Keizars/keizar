4d5e04cb382e1a5b52b871ebdc290402e1a88d45
Him188 <Him188@mamoe.net>
2024-03-01 15:55:05 +0000

    Add SessionManager.self

--- app/src/main/kotlin/client/SessionManager.kt
+++ app/src/main/kotlin/client/SessionManager.kt
@@ -1,23 +1,46 @@
 package org.keizar.android.client
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.transformLatest
+import me.him188.ani.utils.logging.error
 import me.him188.ani.utils.logging.info
 import me.him188.ani.utils.logging.logger
 import org.keizar.android.persistent.TokenRepository
 import org.keizar.android.ui.foundation.BackgroundScope
 import org.keizar.android.ui.foundation.HasBackgroundScope
+import org.keizar.utils.communication.account.User
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import kotlin.time.Duration.Companion.seconds
 
 class SessionManager : KoinComponent, HasBackgroundScope by BackgroundScope() {
     private val tokenRepository: TokenRepository by inject()
+    private val userService: UserService by inject()
+
     private val logger = logger(SessionManager::class)
 
     val token: SharedFlow<String?> = tokenRepository.token.shareInBackground(started = SharingStarted.Eagerly)
     val isLoggedIn: Flow<Boolean> = token.map { it != null }
+
+    /**
+     * Current user's information.
+     */
+    val self: SharedFlow<User> = token.transformLatest {
+        while (true) {
+            try {
+                emit(userService.self())
+                return@transformLatest
+            } catch (e: Exception) {
+                logger.error(e) { "Failed to get self" }
+                delay(5.seconds)
+                continue
+            }
+        }
+    }.shareInBackground(started = SharingStarted.Eagerly)
 
     suspend fun invalidateToken() {
         logger.info { "Invalidating token" }
