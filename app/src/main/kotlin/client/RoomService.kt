bc04675b8f9d4cc5a6fd3116f32065279329394c
JerryZ233 <zyq20033@163.com>
2024-03-01 14:49:21 +0000

    add comments to RoomService

--- app/src/main/kotlin/client/RoomService.kt
+++ app/src/main/kotlin/client/RoomService.kt
@@ -10,15 +10,26 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface RoomService {
+    /**
+     * Create a room with a specific room number and board properties.
+     * Return OK if the room is created successfully.
+     */
     @POST("room/{roomNumber}/create")
     suspend fun createRoom(
         @Path("roomNumber") roomNumber: UInt,
         @Body properties: BoardProperties
     )
 
+    /**
+     * Get the room information of a specified room number.
+     */
     @GET("room/{roomNumber}")
     suspend fun getRoom(@Path("roomNumber") roomNumber: UInt): RoomInfo
 
+    /**
+     * Join a room with given number and user info.
+     * Return OK on success.
+     */
     @POST("room/{roomNumber}/join")
     suspend fun joinRoom(roomNumber: UInt, @Body userInfo: UserInfo): Boolean
 }
