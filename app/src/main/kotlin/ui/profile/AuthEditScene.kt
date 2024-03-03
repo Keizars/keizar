1658d7f9e738a08ee039bce9358c6916c0c1e3e4
Fengkai Liu <fl1121@ic.ac.uk>
2024-03-03 01:06:01 +0000

    Move nicknameEdit in ProfilePage and add dropdownItem for navigating to password edition.

--- app/src/main/kotlin/ui/profile/AuthEditScene.kt
+++ app/src/main/kotlin/ui/profile/AuthEditScene.kt
@@ -7,15 +7,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -26,11 +23,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,13 +38,11 @@ import org.keizar.android.ui.game.mp.room.ConnectingRoomDialog
 
 @Composable
 fun AuthEditScene(
-    initIsPasswordEdit: Boolean,
     onClickBack: () -> Unit,
     onSuccessPasswordEdit: () -> Unit,
-    onSuccessNicknameEdit: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    val vm = remember(initIsPasswordEdit) { AuthEditViewModel(initIsPasswordEdit) }
+    val vm = remember { AuthEditViewModel() }
     Scaffold(
         modifier.fillMaxSize(),
         topBar = {
@@ -76,7 +68,6 @@ fun AuthEditScene(
             AuthEditPage(
                 viewModel = vm,
                 onSuccessPasswordEdit = onSuccessPasswordEdit,
-                onSuccessNicknameEdit = onSuccessNicknameEdit,
                 Modifier.fillMaxSize()
             )
         }
@@ -88,11 +79,9 @@ fun AuthEditScene(
 fun AuthEditPage(
     viewModel: AuthEditViewModel,
     onSuccessPasswordEdit: () -> Unit,
-    onSuccessNicknameEdit: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     val isProcessing by viewModel.isProcessing.collectAsStateWithLifecycle()
-    val isPasswordEdit by viewModel.isPasswordEdit.collectAsStateWithLifecycle()
     if (isProcessing) {
         ConnectingRoomDialog(
             text = {
@@ -110,26 +99,23 @@ fun AuthEditPage(
         ) {
         val passwordError by viewModel.passwordError.collectAsStateWithLifecycle()
         val verifyPasswordError by viewModel.verifyPasswordError.collectAsStateWithLifecycle()
-        val nicknameError by viewModel.nicknameError.collectAsStateWithLifecycle()
 
         // is password edit
-        AnimatedVisibility(isPasswordEdit) {
-            Row(
-                modifier = Modifier.padding(10.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                OutlinedTextField(
-                    value = viewModel.password.value,
-                    onValueChange = { viewModel.setPassword(it) },
-                    isError = (passwordError != null),
-                    label = { Text("New Password") },
-                    shape = RoundedCornerShape(8.dp),
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Password,
-                    ),
-                    visualTransformation = PasswordVisualTransformation('*')
-                )
-            }
+        Row(
+            modifier = Modifier.padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            OutlinedTextField(
+                value = viewModel.password.value,
+                onValueChange = { viewModel.setPassword(it) },
+                isError = (passwordError != null),
+                label = { Text("New Password") },
+                shape = RoundedCornerShape(8.dp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                ),
+                visualTransformation = PasswordVisualTransformation('*')
+            )
         }
         AnimatedVisibility(passwordError != null) {
             passwordError?.let {
@@ -141,74 +127,24 @@ fun AuthEditPage(
             }
         }
 
-        AnimatedVisibility(isPasswordEdit) {
-            Row(
-                modifier = Modifier.padding(10.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                OutlinedTextField(
-                    value = viewModel.verifyPassword.value,
-                    onValueChange = { viewModel.setVerifyPassword(it) },
-                    isError = (verifyPasswordError != null),
-                    label = { Text("Verify New Password") },
-                    shape = RoundedCornerShape(8.dp),
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Password,
-                    ),
-                    visualTransformation = PasswordVisualTransformation('*')
-                )
-            }
+        Row(
+            modifier = Modifier.padding(10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            OutlinedTextField(
+                value = viewModel.verifyPassword.value,
+                onValueChange = { viewModel.setVerifyPassword(it) },
+                isError = (verifyPasswordError != null),
+                label = { Text("Verify New Password") },
+                shape = RoundedCornerShape(8.dp),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                ),
+                visualTransformation = PasswordVisualTransformation('*')
+            )
         }
         AnimatedVisibility(verifyPasswordError != null) {
             verifyPasswordError?.let {
-                Text(
-                    text = it,
-                    fontSize = errorFontSize,
-                    color = Color.Red,
-                )
-            }
-        }
-
-        // is nickname and avatar edit
-        AnimatedVisibility(!isPasswordEdit) {
-            Row(
-                modifier = Modifier.padding(10.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                val curNickname = viewModel.getCurrentNickname()
-                if (curNickname == "") {
-                    Text(
-                        text = "Current name: $curNickname",
-                        fontSize = 16.sp,
-                    )
-                } else {
-                    Text(
-                        text = "Come you set your nickname!",
-                        fontSize = 16.sp,
-                    )
-                }
-            }
-        }
-
-        AnimatedVisibility(!isPasswordEdit) {
-            Row(
-                modifier = Modifier.padding(10.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                OutlinedTextField(
-                    value = viewModel.nickname.value,
-                    onValueChange = { viewModel.setNickname(it) },
-                    isError = (nicknameError != null),
-                    label = { Text("New nickName") },
-                    shape = RoundedCornerShape(8.dp),
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Text,
-                    )
-                )
-            }
-        }
-        AnimatedVisibility(nicknameError != null) {
-            nicknameError?.let {
                 Text(
                     text = it,
                     fontSize = errorFontSize,
@@ -229,11 +165,7 @@ fun AuthEditPage(
                         }
                         if (result) {
                             withContext(Dispatchers.Main) {
-                                if (isPasswordEdit) {
-                                    onSuccessPasswordEdit()
-                                } else {
-                                    onSuccessNicknameEdit()
-                                }
+                                onSuccessPasswordEdit()
                             }
                         }
                     }
@@ -244,34 +176,6 @@ fun AuthEditPage(
         ) {
             Text("Update Account")
         }
-
-        val highlightStyle =
-            SpanStyle(
-                color = MaterialTheme.colorScheme.primary,
-                textDecoration = TextDecoration.Underline
-            )
-        val passwordEditMessage = remember(highlightStyle) {
-            buildAnnotatedString {
-                pushStyle(highlightStyle)
-                append("Change nickname")
-                pop()
-            }
-        }
-
-        val nicknameAvatarEditMessage = remember(highlightStyle) {
-            buildAnnotatedString {
-                pushStyle(highlightStyle)
-                append("Change password")
-                pop()
-            }
-        }
-
-        ClickableText(
-            text = if (isPasswordEdit) passwordEditMessage else nicknameAvatarEditMessage,
-            onClick = { viewModel.onClickSwitch() },
-            style = LocalTextStyle.current.copy(color = MaterialTheme.colorScheme.onBackground),
-            modifier = Modifier.padding(top = 8.dp)
-        )
     }
 }
 
@@ -281,27 +185,11 @@ fun AuthEditPage(
 private fun PreviewNickname() {
     ProvideCompositionalLocalsForPreview {
         AuthEditScene(
-            initIsPasswordEdit = false,
             onClickBack = {},
             onSuccessPasswordEdit = {},
-            onSuccessNicknameEdit = {},
             Modifier
         )
     }
 }
 
-@Preview
-@Preview(device = Devices.TABLET)
-@Composable
-private fun PreviewPassword() {
-    ProvideCompositionalLocalsForPreview {
-        AuthEditScene(
-            initIsPasswordEdit = true,
-            onClickBack = {},
-            onSuccessPasswordEdit = {},
-            onSuccessNicknameEdit = {},
-            Modifier
-        )
-    }
-}
 
