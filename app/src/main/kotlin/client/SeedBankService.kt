d358842562dd13c7d8d67d1d26f2559b25596b82
JerryZ233 <zyq20033@163.com>
2024-03-04 17:44:24 +0000

    fix SeedBankService

--- app/src/main/kotlin/client/SeedBankService.kt
+++ app/src/main/kotlin/client/SeedBankService.kt
@@ -1,22 +1,17 @@
 package org.keizar.android.client
 
-import org.keizar.utils.communication.account.AuthRequest
-import org.keizar.utils.communication.account.AuthResponse
-import org.keizar.utils.communication.account.User
-import org.keizar.utils.communication.account.UsernameValidityResponse
-import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.Path
 
 interface SeedBankService {
     @GET("seed-bank")
     suspend fun getSeeds(): List<String>
 
     @POST("seed-bank/{seed}")
-    suspend fun addSeed(@Body seed: String)
+    suspend fun addSeed(@Path("seed") seed: String)
 
     @DELETE("seed-bank/{seed}")
-    suspend fun removeSeed(@Body seed: String)
+    suspend fun removeSeed(@Path("seed") seed: String)
 }
