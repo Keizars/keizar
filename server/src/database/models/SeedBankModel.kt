0fb32bf7442e43e172e75bc97d6f7ddea4d8f356
Him188 <Him188@mamoe.net>
2024-03-05 16:48:00 +0000

    Fix SeedBankModel

--- server/src/database/models/SeedBankModel.kt
+++ server/src/database/models/SeedBankModel.kt
@@ -1,9 +1,6 @@
 package org.keizar.server.database.models
 
-import org.bson.codecs.pojo.annotations.BsonId
-
 data class SeedBankModel(
-    @BsonId
     val userId: String, // Foreign Key -> Users, Index
     val gameSeed: String,
 )
