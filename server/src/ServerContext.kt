9b0ebcd80152b42237bcfb50b838d295b114380c
JerryZ233 <zyq20033@163.com>
2024-03-06 19:41:55 +0000

    add comments

--- server/src/ServerContext.kt
+++ server/src/ServerContext.kt
@@ -2,6 +2,7 @@ package org.keizar.server
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import org.keizar.server.database.DatabaseManager
 import org.keizar.server.database.InMemoryDatabaseManagerImpl
 import org.keizar.server.database.MongoDatabaseManagerImpl
 import org.keizar.server.modules.AccountModuleImpl
@@ -13,6 +14,7 @@ import org.keizar.server.modules.InMemoryAvatarStorage
 import org.keizar.server.modules.SeedBankModuleImpl
 import org.keizar.server.utils.AesEncoder
 import org.keizar.server.utils.AuthTokenConfig
+import org.keizar.server.utils.AuthTokenManager
 import org.keizar.server.utils.AuthTokenManagerImpl
 import org.slf4j.Logger
 import java.security.SecureRandom
@@ -21,13 +23,22 @@ import kotlin.time.Duration.Companion.days
 /**
  * The [ServerContext] class represents a context object for the server application in order to
  * achieve dependency injection. It contains all the service modules used by the server.
+ *
+ * Instances can be created using the constructor or the [setupServerContext] function.
+ *
+ * @param parentCoroutineScope The [CoroutineScope] used to launch coroutines in the server application.
+ * @param logger The [org.slf4j.Logger] instance used by the server application to log messages.
+ * @param env The environment variables used by the server application.
  */
-class ServerContext(
+class ServerContext internal constructor(
     parentCoroutineScope: CoroutineScope,
     logger: Logger,
     env: EnvironmentVariables,
 ) {
-    private val databaseManager = if (env.testing) {
+    /**
+     * The [DatabaseManager] instance used by the service modules to interact with the database.
+     */
+    private val databaseManager: DatabaseManager = if (env.testing) {
         InMemoryDatabaseManagerImpl()
     } else {
         MongoDatabaseManagerImpl(
@@ -36,7 +47,11 @@ class ServerContext(
         )
     }
 
-    val authTokenManager = AuthTokenManagerImpl(
+    /**
+     * The [AuthTokenManager] instance used by the security plugin and the [accounts] module
+     * to create and validate authentication tokens.
+     */
+    val authTokenManager: AuthTokenManager = AuthTokenManagerImpl(
         config = AuthTokenConfig(
             secret = env.tokenSecret?.toByteArray() ?: generateSecureRandomBytes(),
             expirationTime = 7.days.inWholeMilliseconds,
@@ -44,22 +59,37 @@ class ServerContext(
         encoder = AesEncoder()
     )
 
-    init {
-        parentCoroutineScope.launch { databaseManager.initialize() }
-    }
-
+    /**
+     * The [AvatarStorage] instance used by the [accounts] module to store and retrieve user avatars.
+     */
     private val avatarStorage: AvatarStorage = if (env.testing) {
         InMemoryAvatarStorage()
     } else {
         AwsAvatarStorage()
     }
 
+    init {
+        parentCoroutineScope.launch { databaseManager.initialize() }
+    }
+
+    /**
+     * The service modules of the server application, containing the business logic
+     * of the server used by the HTTP routing layer.
+     * Uses the [databaseManager] to interact with the database.
+     */
     val gameRooms = GameRoomsModuleImpl(parentCoroutineScope.coroutineContext, logger)
     val seedBank = SeedBankModuleImpl(databaseManager)
     val accounts = AccountModuleImpl(databaseManager, authTokenManager, avatarStorage)
     val gameData = GameDataModuleImpl(databaseManager)
 }
 
+/**
+ * Function to create a [ServerContext] instance.
+ *
+ * @param coroutineScope The [CoroutineScope] used to launch coroutines in the server application.
+ * @param logger The [org.slf4j.Logger] instance used by the server application to log messages.
+ * @param env The environment variables used by the server application. By default, it reads the system environment variables.
+ */
 internal fun setupServerContext(
     coroutineScope: CoroutineScope,
     logger: Logger,
@@ -68,6 +98,9 @@ internal fun setupServerContext(
     return ServerContext(coroutineScope, logger, env)
 }
 
+/**
+ * Function that generates a secure random 32-byte array to be used as a secret for the [AuthTokenManager].
+ */
 private fun generateSecureRandomBytes(): ByteArray {
     val bytes = ByteArray(32)
     SecureRandom().nextBytes(bytes)
--- server/src/utils/AuthTokenManager.kt
+++ server/src/utils/AuthTokenManager.kt
@@ -2,11 +2,29 @@ package org.keizar.server.utils
 
 import java.util.*
 
+/**
+ * A manager for creating and validating authentication tokens.
+ * Created by and accessed from the [ServerContext].
+ */
 interface AuthTokenManager {
+    /**
+     * Creates an authentication token for the given user ID.
+     */
     fun createToken(userId: String): String
+
+    /**
+     * Check the validity of the given token and return the user ID if it is valid.
+     */
     fun matchToken(token: String): String?
 }
 
+/**
+ * An implementation of the [AuthTokenManager] interface that uses a given [Encoder] to
+ * encrypt the username and the expiration time into the token.
+ *
+ * @param config The configuration for the authentication token manager. It contains the token secret and the expiration time.
+ * @param encoder The [Encoder] used to encrypt and decrypt the String.
+ */
 class AuthTokenManagerImpl(
     private val config: AuthTokenConfig,
     private val encoder: Encoder,
@@ -33,6 +51,9 @@ class AuthTokenManagerImpl(
     }
 }
 
+/**
+ * A helper extension function to convert a String to a UUID or return null if it is not a valid UUID.
+ */
 fun String.formatToUuidOrNull(): UUID? {
     return try {
         UUID.fromString(this)
@@ -41,7 +62,13 @@ fun String.formatToUuidOrNull(): UUID? {
     }
 }
 
+/**
+ * A configuration class for the [AuthTokenManager].
+ *
+ * @param secret The secret used to encrypt and decrypt the token.
+ * @param expirationTime The time in milliseconds after which the token will expire. If it is set to -1, the token will never expire.
+ */
 class AuthTokenConfig(
     val secret: ByteArray,
-    val expirationTime: Long
+    val expirationTime: Long,
 )
